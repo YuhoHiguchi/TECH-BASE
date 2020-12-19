7a9bb00b9ded15bb7ac0a8cab6a53e42005fc27e
YuhoHiguchi <75192207+YuhoHiguchi@users.noreply.github.com>
2020-12-19 17:17:09 +0900

    Update mission_5-1.php

--- mission_5-1.php
+++ mission_5-1.php
@@ -11,23 +11,24 @@
 </head>
 
 <body>
-    <?php
-    //データベースに接続
-    $dsn = 'データベース名';
-	$user = 'ユーザー名';
-	$password = 'パスワード';
-	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        <?php
+        //データベースに接続
+        $dsn = 'データベース名';
+        $user = 'ユーザー名';
+        $password = 'パスワード';
+        $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	
-	//テーブルの作成
-	$sql = "CREATE TABLE IF NOT EXISTS tbtest"
-	."("
-	."id INT AUTO_INCREMENT PRIMARY KEY,"
-	."name char(32),"
-	."comment TEXT,"
-	."date TEXT,"
-	."pass TEXT"
-	.");";
-	$stmt = $pdo -> query($sql);
+        //テーブルの作成
+        $sql = "CREATE TABLE IF NOT EXISTS tbtest"
+        ."("
+        ."id INT AUTO_INCREMENT PRIMARY KEY,"
+        ."name char(32),"
+        ."comment TEXT,"
+        ."date TEXT,"
+        ."pass TEXT"
+        .");";
+        $stmt = $pdo -> query($sql);
+	
 	
 	//Post受信
 	$name = $_POST["name"];
@@ -77,21 +78,21 @@
 	    //パスワードの抽出・比較
 	    $id = $deleteid;
 	    $sql = 'SELECT * FROM tbtest WHERE id=:id ';
-        $stmt = $pdo -> prepare($sql);                
-        $stmt -> bindParam(':id', $id, PDO::PARAM_INT); 
-        $stmt -> execute();     
-        $results = $stmt -> fetchAll();
-        foreach($results as $row)
-        {
-            if($row['pass'] == $deletepass)
+            $stmt = $pdo -> prepare($sql);                
+            $stmt -> bindParam(':id', $id, PDO::PARAM_INT); 
+            $stmt -> execute();     
+            $results = $stmt -> fetchAll();
+            foreach($results as $row)
             {
-                //削除実行
-                $sql = 'delete from tbtest where id=:id';
+                if($row['pass'] == $deletepass)
+                {
+                    //削除実行
+                    $sql = 'delete from tbtest where id=:id';
 	            $stmt = $pdo -> prepare($sql);
 	            $stmt -> bindParam(':id', $id, PDO::PARAM_INT);
 	            $stmt -> execute();
+                }
             }
-        }
 	}
 	
 	//編集フォーム
@@ -100,21 +101,21 @@
 	    //パスワードの抽出・比較
 	    $id = $editid;
 	    $sql = 'SELECT * FROM tbtest WHERE id=:id ';
-        $stmt = $pdo -> prepare($sql);
-        $stmt -> bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt -> execute();
-        $results = $stmt -> fetchAll();
-        foreach($results as $row)
-        {
-            if($row['pass'] == $editpass)
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt -> execute();
+            $results = $stmt -> fetchAll();
+            foreach($results as $row)
             {
-                //編集する名前・コメントの取得
-                $editname = $row['name'];
-                $editcomment = $row['comment'];
-                //編集中番号の定義
-                $editingid = $editid;
+                if($row['pass'] == $editpass)
+                {
+                    //編集する名前・コメントの取得
+                    $editname = $row['name'];
+                    $editcomment = $row['comment'];
+                    //編集中番号の定義
+                    $editingid = $editid;
+                }
             }
-        }
 	}
 	
 	//掲示板の表示（全部）
@@ -130,27 +131,28 @@
 	}
 	echo "<hr>";
 	
-    ?>
+        ?>
     
-    <form action="" method="post">
+        <form action="" method="post">
         【投稿フォーム】<br>
         名前：　　　<input type="text" name="name" value="<?php echo $editname; ?>" placeholder=""> <br>
         コメント：　<input type="text" name="comment" value="<?php echo $editcomment; ?>" placeholder=""> <br>
         パスワード：<input type="text" name="pass" placeholder="">　<input type="submit" name="submit"> <br>
         <input type="hidden" name="editingid" value="<?php echo $editingid; ?>" placeholder="編集中番号">
-    </form>
-    <br>
-    <form action="" method="post">
+        </form>
+        <br>
+        <form action="" method="post">
         【削除フォーム】<br>
         削除番号：　<input type="number" name="deleteid" placeholder=""> <br>
         パスワード：<input type="text" name="deletepass" placeholder="">　<input type="submit" value="削除"> <br>
-    </form>
-    <br>
-    <form action="" method="post">
+        </form>
+        <br>
+        <form action="" method="post">
         【編集フォーム】<br>
         編集番号：　<input type="number" name="editid" placeholder=""> <br>
         バスワード：<input type="text" name="editpass" placeholder="">　<input type="submit" value="編集"> <br>
-    </form>
+        </form>
 
 </body>
+	
 </html>
